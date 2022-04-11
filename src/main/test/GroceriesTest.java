7272564caf2394e44757117fa042e345583a1be5
Clyde Broderick <clyderick3@gmail.com>
2022-04-11 11:18:02 -0400

    Formatting tests.

---
+++ src/main/test/GroceriesTest.java
@@ -0,0 +1,76 @@
+import junit.framework.TestCase;
+
+public class GroceriesTest extends TestCase {
+
+    public void testTestGetName() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testGetPrice() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testGetType() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testGetExpirationDate() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testTestSetName() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testSetPrice() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testSetType() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    public void testSetExpirationDate() {
+        //Given
+
+        //When
+
+        //Then
+
+    }
+}
