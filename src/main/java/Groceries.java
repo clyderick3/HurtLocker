1ed04a282c6430d0f38fa2add79ef91a62d8d6c1
Clyde Broderick <clyderick3@gmail.com>
2022-04-11 10:50:14 -0400

    Did my this under constructor.

--- src/main/java/Groceries.java
+++ src/main/java/Groceries.java
@@ -6,6 +6,10 @@ public class Groceries {
     private String expirationDate;
 
     public Groceries(String name, float price, String type, String expirationDate){
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.expirationDate = expirationDate;
 
     }
 }
