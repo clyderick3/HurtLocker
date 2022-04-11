f6ba8de0225f00b9fca788a73aa93d722863b746
Clyde Broderick <clyderick3@gmail.com>
2022-04-11 15:01:18 -0400

    Made list of strings in Main. Identified misspelled words.

---
+++ src/main/java/Jerkson.java
@@ -0,0 +1,4 @@
+public class Jerkson {
+
+    private
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+    public List<String> listings = new ArrayList<>();
 
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
@@ -13,5 +18,13 @@ public class Main {
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
 
+    }
+
+    public String fixMilk(String input){
+        try{
+            Pattern pattern = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            String milk = matcher.
+        }
     }
 }
