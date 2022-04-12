faf17dd6ae39c614139290ef12f278c52568e96d
Clyde Broderick <clyderick3@gmail.com>
2022-04-12 18:48:32 -0400

    Created Grocery Parser.

---
+++ src/main/java/GroceryParser.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroceryParser {
+
+    //Here I should separate the words using the punctuation marks provided.
+    private List<String> groceryList = new ArrayList<>();
+
+}
--- src/main/java/Jerkson.java
+++
@@ -1,4 +0,0 @@
-public class Jerkson {
-
-    private
-}
