ca83053cef9182add7ec9ff84f2998ef93b5ed9e
Clyde Broderick <clyderick3@gmail.com>
2022-04-12 20:54:46 -0400

    Created parser for punctuations.

--- src/main/java/GroceryParser.java
+++ src/main/java/GroceryParser.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GroceryParser {
 
     //Here I should separate the words using the punctuation marks provided.
     private List<String> groceryList = new ArrayList<>();
 
+    public static boolean hasPunctuation(String input){
+        Pattern pattern = Pattern.compile("(:@)|(:!)|(:%)|(:;)(:\\*)");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    public static List<String> kvString(String stringFound, String rawData){
+        return new ArrayList<>(Arrays.asList(rawData.split(stringFound)));
+    }
 }
